243701e3c4a3726e14c4f8012796331ce881f542
bryanzhao08 <guavatyphoon@gmail.com>
2025-08-06 19:12:07 -0700

    bug fixing

---  script.js
+++  script.js
@@ -8,43 +8,40 @@ button.addEventListener("click", () => {
   if (!username) return;
 
   fetch(url + username)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error("User not found");
-      }
-      return res.json();
-    })
+    .then(res => res.json())
     .then(data => {
-      noResults.style.display = "none";
-      updateProfile(data);
+      if (data.message === "Not Found") {
+        noResults.style.display = "block";
+      } else {
+        noResults.style.display = "none";
+        updateProfile(data);
+      }
     })
     .catch(err => {
-      console.error(err);
+      console.error("Fetch error:", err);
       noResults.style.display = "block";
     });
 });
 
 function updateProfile(data) {
-  document.getElementById("avatar").src = data.avatar_url || "";
-  document.getElementById("name").innerText = data.name || data.login || "Unknown User";
+  document.getElementById("avatar").src = data.avatar_url;
+  document.getElementById("name").innerText = data.name || data.login;
   document.getElementById("user").innerText = `@${data.login}`;
-  document.getElementById("user").href = data.html_url || "#";
+  document.getElementById("user").href = data.html_url;
   document.getElementById("date").innerText = `Joined: ${new Date(data.created_at).toDateString()}`;
   document.getElementById("bio").innerText = data.bio || "No bio available.";
-  document.getElementById("repos").innerText = data.public_repos ?? "0";
-  document.getElementById("followers").innerText = data.followers ?? "0";
-  document.getElementById("following").innerText = data.following ?? "0";
+  document.getElementById("repos").innerText = data.public_repos;
+  document.getElementById("followers").innerText = data.followers;
+  document.getElementById("following").innerText = data.following;
   document.getElementById("location").innerText = data.location || "Not available";
   document.getElementById("page").innerText = data.blog || "Not available";
   document.getElementById("page").href = data.blog || "#";
   document.getElementById("twitter").innerText = data.twitter_username || "Not available";
-  document.getElementById("twitter").href = data.twitter_username
-    ? `https://twitter.com/${data.twitter_username}`
-    : "#";
+  document.getElementById("twitter").href = data.twitter_username ? `https://twitter.com/${data.twitter_username}` : "#";
   document.getElementById("company").innerText = data.company || "Not available";
 }
 
-// Dark Mode Toggle
+// DARK MODE TOGGLE
 const modeBtn = document.getElementById("btn-mode");
 const modeText = document.getElementById("mode-text");
 
